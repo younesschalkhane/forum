37e04bf7e3c0e9c063f3d489a751b52a4db92b96
younesschalkhane <chalkhane.youness@hotmail.com>
2026-03-12 12:12:44 +0100

    final

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-let 
+function login(){
+
+let email = document.getElementById("email").value;
+let password = document.getElementById("password").value;
+
+const correctEmail = "younessly@gmail.com";
+const correctPassword = "98765ly";
+
+
+
+
+
+if(email !== correctEmail){
+message.innerText = "Email incorrect";
+}
+
+let message = document.getElementById("message");
+
+
+if(password !== correctPassword){
+message.innerText = "Password incorrect";
+}
+
+let message = document.getElementById("message");
+
+
+
+else{
+message.innerText = "Login successful";
+}
+
+}
